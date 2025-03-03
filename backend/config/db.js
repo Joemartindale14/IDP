65c7ffc7c6378cbaf1bbdf9c3a60c681376ae163
Joemartindale14 <mar23006620@heartofyorkshire.ac.uk>
2025-03-03 15:21:51 +0000

    Updated version v.1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://JoeMartindale:Resetmypassword1!@cluster0.inhx4.mongodb.net/IDP").then(()=>console.log("Database Connected"));
+    await mongoose.connect("mongodb+srv://JoeMartindale:Resetmypassword1@cluster0.inhx4.mongodb.net/IDP").then(()=>console.log("Database Connected"));
 };
